289361023caebe19c51bed72a3cc92ab88d9d065
Chris <cachou@umich.edu>
2018-06-27 12:25:38 -0500

    initial storyboard

---
+++ PLC/TaskTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  InitiativesViewController.swift
+//  PLC
+//
+//  Created by Chris on 6/25/18.
+//  Copyright © 2018 Chris Chou. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewController: UITableViewController {
+    
+    let tasks = ["task #1"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath) as! TaskTableViewCell
+        cell.taskTitle.text = "World Cup Watch Party"
+        cell.taskLocation.text = "Main Kitchen"
+        cell.taskTime.text = "1d ago"
+        cell.taskReplies.text = "3 replies"
+        cell.taskTag.text = "#participate"
+        return cell
+    }
+}
+
